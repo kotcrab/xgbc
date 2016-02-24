4b9beed45a4098e1763e238478e013a874fb51f9
Paweł Pastuszak <kotcrab@gmail.com>
2016-02-24 21:42:46 +0100

    Implement memory map

--- src/main/com/kotcrab/xgbc/Emulator.kt
+++ src/main/com/kotcrab/xgbc/Emulator.kt
@@ -7,11 +7,27 @@ class Emulator(romFile: FileHandle) {
     val rom = Rom(romFile)
     val cpu = Cpu(this)
 
-    fun read(addr: Int): Byte {
-        if (addr < 0x8000)
-            return rom.read(addr);
+    val ram: ByteArray = ByteArray(0x2000)
+    val vram: ByteArray = ByteArray(0x2000)
+    val oam: ByteArray = ByteArray(0x80)
+    val internalRam: ByteArray = ByteArray(0xA0)
 
-        throw EmulatorException("Unsupported address: " + addr);
+    /** Interrupt Enable */
+    var ie: Byte = 0
+
+    fun read(addr: Int): Byte {
+        when (addr) {
+            in 0x0000..0x8000 - 1 -> return rom.read(addr);
+            in 0x8000..0xA000 - 1 -> return vram[addr - 0x8000];
+            in 0xA000..0xC000 - 1 -> throw EmulatorException("Switchable RAM bank not implemented. Address: " + addr);
+            in 0xC000..0xE000 - 1 -> return ram[addr - 0xC000];
+            in 0xE000..0xFE00 - 1 -> return ram[addr - 0xE000]; //ram echo
+            in 0xFE00..0xFEA0 - 1 -> return oam[addr - 0xFE00];
+            in 0xFEA0..0xFF80 - 1 -> throw EmulatorException("IO not implemented. Address: " + addr);
+            in 0xFE80..0xFFFF - 1 -> return internalRam[addr - 0xFE80]
+            0xFFFF -> return ie;
+            else -> throw EmulatorException("Unsupported address: " + addr)
+        }
     }
 
     fun readInt(addr: Int): Int {
