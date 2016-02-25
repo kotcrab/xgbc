259cc04b8ffba0ebbebfd7064f0c2fb7051eaa93
Paweł Pastuszak <kotcrab@gmail.com>
2016-02-25 22:28:21 +0100

    Implement LD n, A. Method names refactor

--- src/main/com/kotcrab/xgbc/OpCodesProcessor.kt
+++ src/main/com/kotcrab/xgbc/OpCodesProcessor.kt
@@ -5,38 +5,51 @@ class OpCodesProcessor(private val emulator: Emulator) {
     private val cpu = emulator.cpu
 
     /** Loads 8 bit value into cpu register. */
-    fun ld8(reg: Int, value: Byte) {
+    private fun ld8ValueToReg(reg: Int, value: Byte) {
         cpu.writeReg(reg, value)
     }
 
-    /** Loads immediate 8 bit value into cpu register. */
-    fun ld8Imm(reg: Int) {
-        ld8(reg, emulator.read(cpu.pc + 1))
-    }
-
     /** Copies value from cpu's reg2 into reg1. */
-    fun ld8(reg1: Int, reg2: Int) {
+    fun ld8RegToReg(reg1: Int, reg2: Int) {
         cpu.writeReg(reg1, cpu.readReg(reg2))
     }
 
     /** Loads 8 bit value from memory address pointed by reg16 into reg */
-    fun ld8FromReg16Addr(reg: Int, reg16: Int) {
+    fun ld8Reg16AddrToReg(reg: Int, reg16: Int) {
         cpu.writeReg(reg, emulator.read(cpu.readReg16(reg16)))
     }
 
-    /** Loads 8 bit value from reg into memory address pointed by reg16 */
-    fun ld8ToReg16Addr(reg16: Int, reg: Int) {
+    /** Saves 8 bit value from reg into memory address pointed by reg16 */
+    fun ld8RegToReg16Addr(reg16: Int, reg: Int) {
         emulator.write(cpu.readReg16(reg16), cpu.readReg(reg))
     }
 
     /** Loads 8 bit value from addr into reg */
-    fun ld8FromAddr(reg: Int, addr: Int) {
+    private fun ld8AddrToReg(reg: Int, addr: Int) {
         cpu.writeReg(reg, emulator.read(addr))
     }
 
+    /** Saves 8 bit value from reg to addr */
+    private fun ld8RegToAddr(addr: Int, reg: Int) {
+        emulator.write(addr, cpu.readReg(reg))
+    }
+
+    // Immediate 8 bit operations
+
+    /** Loads immediate 8 bit value into cpu register. */
+    fun ld8ImmValueToReg(reg: Int) {
+        ld8ValueToReg(reg, emulator.read(cpu.pc + 1))
+    }
+
     /** Loads 8 bit value from immediate address into reg */
-    fun ld8Imm16(reg: Int) {
+    fun ld8Imm16AddrToReg(reg: Int) {
         val fromAddr = emulator.read16(cpu.pc + 1);
-        ld8FromAddr(reg, emulator.read16(fromAddr))
+        ld8AddrToReg(reg, fromAddr)
+    }
+
+    /** Saves 8 bit value from reg into immediate address */
+    fun ld8RegToImm16Addr(reg: Int) {
+        val toAddr = emulator.read16(cpu.pc + 1);
+        ld8RegToAddr(toAddr, reg);
     }
 }
