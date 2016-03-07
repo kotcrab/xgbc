833582daf363780eaa439aa2f7aa6d60e4479c44
Paweł Pastuszak <kotcrab@gmail.com>
2016-03-07 23:07:58 +0100

    Fix imports

--- src/main/com/kotcrab/xgbc/rom/mbc/MBC1.kt
+++ src/main/com/kotcrab/xgbc/rom/mbc/MBC1.kt
@@ -1,8 +1,8 @@
 package com.kotcrab.xgbc.rom.mbc
 
 import com.kotcrab.xgbc.EmulatorException
-import com.kotcrab.xgbc.rom.Rom
 import com.kotcrab.xgbc.isBitSet
+import com.kotcrab.xgbc.rom.Rom
 import com.kotcrab.xgbc.toHex
 
 /** @author Kotcrab */
@@ -10,15 +10,15 @@ class MBC1(private val rom: Rom) : MBC {
     var activeRomBank = 1
     var activeRamBank = 0
 
-    var mode = com.kotcrab.xgbc.rom.MBC1.MBC1Mode.ROM16_RAM8;
+    var mode = MBC1Mode.ROM16_RAM8;
 
     override fun write(addr: Int, value: Byte) {
         println("mbc write ${toHex(addr)}")
         if (addr in 0x6000..0x7FFF) {
             if (value.isBitSet(0)) {
-                mode = com.kotcrab.xgbc.rom.MBC1.MBC1Mode.ROM16_RAM8
+                mode = MBC1Mode.ROM16_RAM8
             } else
-                mode = com.kotcrab.xgbc.rom.MBC1.MBC1Mode.ROM4_RAM32
+                mode = MBC1Mode.ROM4_RAM32
         }
     }
 
